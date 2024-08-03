a200ad5236f15942767bfa1007e189b431f28451
Filipe Ferreira <gestaofilipeferreira@gmail.com>
2024-08-03 18:06:45 -0300

    Mudanças no carrossel, alterações nos links do navegador, etc

--- v1.6/projeto1/js/scripts.js
+++ v1.6/projeto1/js/scripts.js
@@ -60,3 +60,20 @@ sr.reveal('#mainNav',{duration: 2000});
 sr.reveal('.about-section h2',{duration:1500});
 sr.reveal('.about-section p',{duration:3000});
 sr.reveal('.text-carousel',{duration:3000});
+
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function() {
+    nextImage();
+}, 5000);
+
+function nextImage(){
+    count++;
+    if(count>4){
+        count =1;
+    }
+
+    document.getElementById("radio"+count).checked = true;
+}
